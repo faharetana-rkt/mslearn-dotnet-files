2882c895206c69fad6c5edb43f7bf7f8e7a901be
faharetana-rkt <rakotomaharitra@gmail.com>
2026-01-14 11:01:44 +0300

    finished the tutorial

--- Program.cs
+++ Program.cs
@@ -1,12 +1,17 @@
 ﻿using System.IO;
 using System.Collections.Generic;
+using Newtonsoft.Json;
 
-var salesFiles = FindFiles("stores");
+var currentDirectory = Directory.GetCurrentDirectory();
+var storesDirectory = Path.Combine(currentDirectory,"stores");
+var salesTotalDir = Path.Combine(currentDirectory, "salesTotalDir");
+Directory.CreateDirectory(salesTotalDir); // Create the directory
 
-foreach (var file in salesFiles)
-{
-    Console.WriteLine(file);
-}
+var salesFiles = FindFiles(storesDirectory); // Get the JSON files
+
+var salesTotal = CalculateSalestotal(salesFiles); // Calculate the total
+
+File.AppendAllText(Path.Combine(salesTotalDir, "totals.txt"), $"{salesTotal}{Environment.NewLine}");
 
 IEnumerable<string> FindFiles(string folderName)
 {
@@ -14,8 +19,8 @@ IEnumerable<string> FindFiles(string folderName)
     var foundFiles = Directory.EnumerateFiles(folderName, "*", SearchOption.AllDirectories);
     foreach (var file in foundFiles)
     {
-        // The file name will contain the full path, so only check the end of it 
-        if (file.EndsWith("sales.json"))
+        var extension = Path.GetExtension(file);
+        if (extension == ".json")
         {
             salesFiles.Add(file);
         }
@@ -23,5 +28,25 @@ IEnumerable<string> FindFiles(string folderName)
     return salesFiles;
 }
 
+double CalculateSalestotal(IEnumerable<string> salesFiles)
+{
+    double salesTotal = 0;
+    // Loop over each file path in salesFiles
+    foreach (var file in salesFiles)
+    {
+        // Read the contents of the file
+        string salesJson = File.ReadAllText(file);
+        // Parse the contents as JSON
+        // the ? means the value can be null and if it is indeed null the compiler doesn't throw an error
+        SalesData? data = JsonConvert.DeserializeObject<SalesData?>(salesJson);
+        // Add the amount found in the Total field to the salesTotal variable
+        // ?. is a null conditional operator & ?? is a null coalescing operator
+        // basically means if data isn't null, add total or if it is indeed null add 0 to the total
+        salesTotal += data?.Total ?? 0;
+    }
+    return salesTotal;
+}
+
+record SalesData (double Total);
 
 
